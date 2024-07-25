159785b6d153193474aad1f21d94af0f4e7c77ef
EduardoBuenoSilva <78116094+EduardoBuenoSilva@users.noreply.github.com>
2024-07-24 22:47:02 -0300

    Seção 5: Testando a funcionalidade de criação de projeto

---
+++ cypress/e2e/gui/createProject.cy.js
@@ -0,0 +1,20 @@
+import { faker } from '@faker-js/faker' // biblioteca que serve para gerar dados
+
+describe('Create Project', () => {
+  beforeEach(() => {
+    cy.login()
+  })
+
+  it('successfully', () => {
+    const project = {
+      name: `project-${faker.datatype.uuid()}`, //gera as palavras aleatórias - template  literals
+      description: faker.random.words(5) // serão 5 palavras aleatórias
+    }
+
+    cy.gui_createProject(project)
+
+    cy.url().should('be.equal', `${Cypress.config('baseUrl')}/${Cypress.env('user_name')}/${project.name}`)
+    cy.contains(project.name).should('be.visible')
+    cy.contains(project.description).should('be.visible')
+  })
+})
